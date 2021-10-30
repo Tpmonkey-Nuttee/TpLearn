b371c3bb45a32b8857bbef6311f5ee906b655a75
Tpmonkey <nuttee.2548@hotmail.com>
2021-10-30 08:09:07 +0700

    Bugs command

--- exts/utils.py
+++ exts/utils.py
@@ -61,6 +61,39 @@ class Utility(Cog):
         embed.colour = ctx.author.top_role.colour if roles else Colour.blue()
 
         await ctx.send(embed=embed)
+    
+    @command()
+    async def bugs(self, ctx: Context) -> None:
+        """List of known bugs."""
+        embed = Embed(
+            title = "Known Bugs",
+            description = "This embed shows list of known bugs, Please dm the bot if you find more bug.",
+            colour = Colour.dark_red(),
+            timestamp = ctx.message.created_at
+        )
+
+        embed.add_field(
+            name = "1) When using Music command, Sometimes bot doesn't play anything.",
+            value = "This bug is caused by Youtube Service problem, Currently I couldn't find a way to fix it.\nBut you can use leave command and rejoin bot again to fix it.",
+            inline = False
+        )
+
+        embed.add_field(
+            name = "2) KUS news system sometimes sent the same news.",
+            value = "This bug is caused by the website itself. How the news system work is to detect changes on the first announcement page\n"
+            "and sometimes, They deleted/changed the announcement which the bot sees it as a change so it sent the news embed.\n"
+            "Simply, just ignore it. The chance of it is pretty low, but I will keep an eyes on it and will implement new system is needed.",
+            inline = False
+        )
+
+        embed.add_field(
+            name = "3) Bot stopped playing song and quit the voice channel.",
+            value = "There are lots of ways that this bug will happen but 90% of which is caused by bot restarting.\n"
+            "Simply, wait a bit then try rejoining the bot again. (You can also check bot status to see if it's restarting.)",
+            inline = False
+        )
+
+        await ctx.send(embed=embed)
 
     @command(aliases = ('fact', 'tips', 'tip', 'qoute', ))
     async def facts(self, ctx: Context) -> None:
