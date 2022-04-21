98afad4a2d177ee6b09f220c26ebe9b1b3aa444e
Tpmonkey <nuttee.2548@hotmail.com>
2022-04-21 15:52:31 +0700

    Fix 429 when playing too many songs.

--- utils/audio.py
+++ utils/audio.py
@@ -88,18 +88,12 @@ class YTDLSource(discord.PCMVolumeTransformer):
     YTDL_OPTIONS = {
         'format': 'bestaudio/mp3/wav/best/worst/worstaudio',
         'extractaudio': True,
-        'audioformat': 'mp3',
         'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-        'restrictfilenames': True,
         'noplaylist': True,
         'nocheckcertificate': True,
-        'ignoreerrors': False,
-        'logtostderr': False,
-        'quiet': True,
-        'no_warnings': True,
         'default_search': 'auto',
         'source_address': '0.0.0.0',
-        "cookiefile": "com_cookies.txt",
+        "cookiefile": "youtube.com_cookies.txt",
         "cachedir": False,
     }
 
