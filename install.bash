4def5ccd0edbe84cea8ce113ce00439b85b8a34e
Tpmonkey <nuttee.2548@hotmail.com>
2022-05-02 15:02:43 +0700

    Changes install.bash

--- install.bash
+++ install.bash
@@ -5,9 +5,6 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install git
-sudo apt install curl
-
 # Install redis
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 
@@ -18,11 +15,7 @@ sudo apt-get install redis
 sudo apt install python3-poetry
 sudo apt install python3-cachecontrol
 
-# Creating Bot directory
-mkdir TpLearn
-cd TpLearn
-
-git clone https://github.com/Tpmonkey-Nuttee/TpLearn.git
+# Install poetry
 poetry install
 
 echo "Please clone `default_config.json` and rename to `config.json` then set up the config file."
